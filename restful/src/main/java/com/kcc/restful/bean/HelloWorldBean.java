f8d0f68a53bf7cf068edb5cefe8ec37350bd0e87
221B0825 <eunseoyu0825@gmail.com>
2024-08-19 11:07:06 +0900

    feat: init

---
+++ restful/src/main/java/com/kcc/restful/bean/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package com.kcc.restful.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
---
+++ restful/src/test/java/com/kcc/restful/RestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcc.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
