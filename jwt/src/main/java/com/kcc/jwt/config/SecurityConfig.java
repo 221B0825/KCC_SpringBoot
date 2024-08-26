acc3c69e9264cefd34cc67f01ba0eb82bdb69066
221B0825 <eunseoyu0825@gmail.com>
2024-08-26 15:19:13 +0900

    feat & fix: add join & login function

--- jwt/src/main/java/com/kcc/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/kcc/jwt/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -28,13 +29,17 @@ public class SecurityConfig {
     };
 
     @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
 //        http.addFilterBefore(new MyFilter1(), BasicAuthenticationFilter.class);
         http.csrf(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
--- jwt/src/main/java/com/kcc/jwt/filter/JwtAuthenticationFilter.java
+++ jwt/src/main/java/com/kcc/jwt/filter/JwtAuthenticationFilter.java
@@ -1,23 +1,51 @@
 package com.kcc.jwt.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kcc.jwt.config.PrincipalDetail;
+import com.kcc.jwt.model.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
 
+
+    // if trying to "login", then call this method
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        System.out.println("attemptAuthentication");
-        // 1. send username & password
-        return super.attemptAuthentication(request, response);
+        System.out.println("called attemptAuthentication");
+        // 1. get username & password
+        try {
+            ObjectMapper om = new ObjectMapper(); // parsing json data
+            // using Object Mapper
+            User user = om.readValue(request.getInputStream(), User.class);
+            System.out.println("ObjectMapper :: readValue"+user);
+
+            // create AuthToken
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
+
+            // 2. call "loadUserByUserName()" in PrincipalDetailService for authenticate
+            Authentication auth = authenticationManager.authenticate(authenticationToken);
+
+            // 3. get principalDetail
+            PrincipalDetail principalDetail = (PrincipalDetail) auth.getPrincipal();
+            System.out.println("PrincipalDetail :: "+principalDetail);
+            return auth;
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
